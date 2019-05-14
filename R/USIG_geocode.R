d4ece36f5b59fd7daca97507f6234996b0325ea1
Antonio Vazquez Brust <bitsandbricks@users.noreply.github.com>
2019-05-14 12:14:35 -0300

    Update USIG_geocode.R

--- R/USIG_geocode.R
+++ R/USIG_geocode.R
@@ -23,45 +23,46 @@
 #' by Antonio Vazquez Brust
 #' @export
 USIG_geocode <- function(address) {
-
-  base_url <- "http://servicios.usig.buenosaires.gob.ar/normalizar/"
-
-  make_address_query <- function(address) {
-    query <- paste0(base_url,
-                    "?direccion=",
-                    address,
-                    "&geocodificar=TRUE")
-  }
-
-  query <- URLencode(make_address_query(address))
-
-  print(paste("Trying", query))
-
-  results <- tryCatch(GET(query),
-                      error = function(error_message) {return(NULL)})
-
-
-
-  if (!httr:::is.response(results)) {
-    data.frame(address = address, lat = NA, lng = NA, stringsAsFactors = FALSE)
-  } else {
-    results <- content(results)
-    if (length(results$direccionesNormalizadas)) {
-      x <- results$direccionesNormalizadas[[1]]$coordenadas$x
-      y <- results$direccionesNormalizadas[[1]]$coordenadas$y
-      address_normalised <- results$direccionesNormalizadas[[1]]$direccion
-    } else {
-      x <- NA
-      y <- NA
-      address_normalised <- NA
+    
+    base_url <- "http://servicios.usig.buenosaires.gob.ar/normalizar/"
+    
+    make_address_query <- function(address) {
+        query <- paste0(base_url,
+                        "?direccion=",
+                        address,
+                        "&geocodificar=TRUE")
     }
-
-
-    data.frame(address = address,
-               address_normalised = ifelse(is.null(address_normalised), NA, address_normalised),
-               lat = ifelse(is.null(y), NA, y),
-               lng = ifelse(is.null(x), NA, x),
-               stringsAsFactors = FALSE)
-  }
-
+    
+    query <- URLencode(make_address_query(address))
+    
+    print(paste("Trying", query))
+    
+    results <- tryCatch(GET(query),
+                        error = function(error_message) {return(NULL)})
+    
+    
+    
+    if (!httr:::is.response(results)) {
+        data.frame(address = address, address_normalised = NA, 
+                   lat = NA, lng = NA, stringsAsFactors = FALSE)
+    } else {
+        results <- content(results)
+        if (length(results$direccionesNormalizadas)) {
+            x <- results$direccionesNormalizadas[[1]]$coordenadas$x
+            y <- results$direccionesNormalizadas[[1]]$coordenadas$y
+            address_normalised <- results$direccionesNormalizadas[[1]]$direccion
+        } else {
+            x <- NA
+            y <- NA
+            address_normalised <- NA
+        }
+        
+        
+        data.frame(address = address,
+                   address_normalised = ifelse(is.null(address_normalised), NA, address_normalised),
+                   lat = ifelse(is.null(y), NA, y),
+                   lng = ifelse(is.null(x), NA, x),
+                   stringsAsFactors = FALSE)
+    }
+    
 }
